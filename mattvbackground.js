07baa2b4621a8de19aa32c4f8d79b329ccce7657
Matthew Vaughan <mvaughan404@gmail.com>
2024-11-11 17:09:03 -0500

    add resume

--- mattvbackground.js
+++ mattvbackground.js
@@ -11,7 +11,7 @@ class MattVBackground {
         this.banner.src = 'banner.jpg'
 
         // lamdas are wrapped so closure has 'this'
-        this.canvas.addEventListener("mousemove", (e) => this.storeMouseEvent(e))
+        document.addEventListener("mousemove", (e) => this.storeMouseEvent(e))
         window.requestAnimationFrame( () => this.draw() )
     }
     
@@ -33,22 +33,13 @@ class MattVBackground {
     draw() {
         const pos = this.getMousePos()
         
-        /*this.context.fillStyle = "rgb(255,50,0)"
-        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
-        */
         try {
             const aspectR = this.canvas.width/this.canvas.height
             this.context.drawImage(this.banner,0,0,aspectR*550,550,0,0,this.canvas.width,this.canvas.height)
         } catch (e) {
             console.log('banner wasnt loaded')
         }
-
-        /*this.context.fillStyle = "rgb(255,255,255)"
-        this.context.arc(pos.x, pos.y, this.canvas.width/2, 0, 2 * Math.PI)
-        this.context.fill()
-        */
-        
-        
+       
         this.context.lineWidth = this.canvas.width/50 + 1
         for( var i=1; i < 50; i++ ) {
             const r = 3*this.canvas.width/2 - (i*this.canvas.width/50)
@@ -58,7 +49,6 @@ class MattVBackground {
             this.context.stroke()
         }
 
-        // must wrap lambda so 'this' is in closure
         this.window.requestAnimationFrame( () => this.draw() )
     }
 }
