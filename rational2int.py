ac713f35557ab5abcd7e27a42179f4ce007bd478
Matthew Vaughan <mvaughan404@gmail.com>
2024-11-26 18:58:26 -0500

    update python script with binary string mapping

--- rational2int.py
+++ rational2int.py
@@ -13,6 +13,17 @@ A problem equivalent to Cantor's diagonal.
 The variables have seemingly multiple and therefor NO possible values.
 Demonstrating the uncountable nature of the set of irrational numbers.
 
+AND
+
+1-to-1 mapping of binary strings (uncountable) to natural!!!
+This WORKS... I understand that this solution will work on a computer
+because the strings passed 'cant' be infinite. But it provides a consistent
+and unique 1-to-1 mapping
+
+But this will work for any binary strings of a arbitrary finite length
+Explanation and test cases below
+
+Run the code :)
 """
 
 # Previous prime in sequence. example 3 returns 2
@@ -31,6 +42,16 @@ def next_prime(num):
         n = n + 1
     return n
 
+"""
+All the magic is in push and pull prime factors
+example:
+input to push_prime_factors = {2:2, 3:1, 5:1} -- presumably for 60
+output would be             = {3:2, 5:1, 7:1} -- removes the 2, reserving it to encode number of leading zeros or decimal place
+
+pull_prime_factors just reverses the process, returning the new set of factors and the previous value for '2' in a tuple
+input   {2:1, 3:2, 5:3, 7:4}
+output  1, {2:2, 3:3, 5:4}
+"""
 # Increment all primes in factors. example for 10 the factors {2:1, 5:1} become {3:1, 7:1}
 def push_prime_factors(factors):
     newfactors = {}
@@ -82,6 +103,9 @@ def factorsToInt(factors):
         result = result * prime**exp
     return result 
 
+"""
+Mapping of all non irrational reals (rational numbers) to ints and back
+"""
 def rationalToInt( r ):
     if ( r == 0 ):
         return 0
@@ -128,7 +152,73 @@ def intToRational( z ):
     else:
         return r 
 
-# TESTS
+"""
+The best part... this is an 'effective' 1-to-1 mapping of
+binary strings to natural numbers for all binary strings of an arbitrary length...
+meaning it would capture any binary string with a unque mapping to a natural number
+given that the leading number of zeros is FINITE.
+
+--it works for anything in real life--
+
+How is that the case that this works if binary strings are uncountable?
+BECAUSE our real world input is finite we can deal with two dimensions of counting
+
+The reason we can't get irrational numbers mapped is their infinite length...
+We could only map their ever more precise approximations. 
+Binary strings are easier, there is no concept 
+"""
+
+def binary_string_to_nat( binstr ):
+    if binstr == '0':
+        return 2
+    elif binstr == '1':
+        return 1
+    
+    leadingzeros = binstr.index('1')
+    z = int(binstr[leadingzeros:], 2)
+    factors = push_prime_factors(prime_factors(z))
+    factors[2] = leadingzeros
+    
+    return factorsToInt(factors)
+
+def nat_to_binary_string( n ):
+    if n == 1:
+        return '1'
+    if n == 2:
+        return '0'
+    
+    zeros, factors = pull_prime_factors(prime_factors(n))
+    return ('0' * zeros) + bin(factorsToInt(factors))[2:]
+
+passed = True
+bss = []
+for i in range(10000):
+    bs = nat_to_binary_string(i)
+    print( f"{i} : {bs}" )
+    if bs in bss:
+        print(f"Failed for uniqueness on {bs} for {i}")
+        passed = False
+
+# two dimensions counting... both could be infinite
+# this IS what makes it uncountable... this is it's cardinality exposed
+ns = []
+for i in range(10):
+    for j in range(1,10000):
+        bs = ('0' * i) + bin(j)[2:]
+        n = binary_string_to_nat(bs)
+        print(f"{bs} : bin natural {bin(n)}\tnatural {n}" )
+        if n in ns:
+            print(f"Failed for uniqueness on {n} for {bs}")
+            passed = False
+if passed:
+    print("Passed all tests")
+else:
+    print("Failed for unique mapping")
+
+""""
+# TESTS CASES FOR RATIONAL TO NATURAL USING THE SAME METHOD
+# Similar to the above, this works for all finit precision numbers
+# So you'll get one for any precision of irrational number you require 
 passed = True
 intvals = []
 for i in [x/240 for x in range(0,300,30)]:
@@ -170,5 +260,5 @@ if passed:
     print("Passed all tests")
 else:
     print("Failed")
-
+"""
 
